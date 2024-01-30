4f4ec4651d25399fbfeedfdaf30ebf8c99671a3b
carolina-lima <lima.carolinadelima@gmail.com>
2024-01-30 00:02:33 -0300

    feat: upload game images

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
+
+
 SECRET_KEY = 'alura'
 
 SQLALCHEMY_DATABASE_URI = \
@@ -8,3 +11,5 @@ SQLALCHEMY_DATABASE_URI = \
         servidor = 'localhost',
         database = 'jogoteca'
     )
+
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads' #dunder file
